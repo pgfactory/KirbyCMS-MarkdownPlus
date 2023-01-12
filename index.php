b56b8cce468fcecee271caa4057f6d6ce5f67adf
Dieter Stokar <inbox@usility.ch>
2023-01-12 21:50:54 +0100

    index.php: - bugfix case no text from meta-file available

--- index.php
+++ index.php
@@ -5,7 +5,10 @@ include_once __DIR__ . '/vendor/autoload.php';
 Kirby::plugin('usility/markdownplus', [
     'components' => [
         'markdown' => function (Kirby $kirby, string $text = null) {
-            $md = new Usility\MarkdownPlus\MarkdownPlus($kirby);
+            if (!$text) {
+                return '';
+            }
+            $md = new Usility\MarkdownPlus\MarkdownPlus();
             return $md->compile($text);
         }
     ]

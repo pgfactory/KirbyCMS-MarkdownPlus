c45bbfef55e5b699dfb061e32a4e0798be582bd1
Dieter Stokar <zurisee@gmail.com>
2023-03-14 15:00:52 +0100

    modif for Composer installation

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-include_once __DIR__ . '/vendor/autoload.php';
+@include_once __DIR__ . '/vendor/autoload.php';
+include_once __DIR__ . '/src/MarkdownPlus.php';
+include_once __DIR__ . '/src/MdPlusHelper.php';
 
 Kirby::plugin('usility/markdownplus', [
     'components' => [
